f7997c7000284543b174b547a391e1244d17a940
DijhonsDeveloper <dijhonsdeveloper1023@gmail.com>
2022-03-06 22:05:48 -0500

    FooterOk

--- js/script.js
+++ js/script.js
@@ -1,37 +1,42 @@
-let navbar = document.querySelector('.header .navbar')
+let navbar = document.querySelector(".header .navbar");
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.add('active');
-}
-
-document.querySelector('#close-navbar').onclick = () =>{
-    navbar.classList.remove('active');
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.add("active");
 };
 
-let registerBtn = document.querySelector('.account-form .register-btn');
-let loginBtn = document.querySelector('.account-form .login-btn');
-
-registerBtn.onclick = () =>{
-    registerBtn.classList.add('active');
-    loginBtn.classList.remove('active');
-    document.querySelector('.account-form .login-form').classList.remove('active');
-    document.querySelector('.account-form .register-form').classList.add('active');
+document.querySelector("#close-navbar").onclick = () => {
+  navbar.classList.remove("active");
 };
 
-loginBtn.onclick = () =>{
-    registerBtn.classList.remove('active');
-    loginBtn.classList.add('active');
-    document.querySelector('.account-form .login-form').classList.add('active');
-    document.querySelector('.account-form .register-form').classList.remove('active');
+let registerBtn = document.querySelector(".account-form .register-btn");
+let loginBtn = document.querySelector(".account-form .login-btn");
+
+registerBtn.onclick = () => {
+  registerBtn.classList.add("active");
+  loginBtn.classList.remove("active");
+  document
+    .querySelector(".account-form .login-form")
+    .classList.remove("active");
+  document
+    .querySelector(".account-form .register-form")
+    .classList.add("active");
 };
 
-
-let accountForm = document.querySelector('.account-form')
-
-document.querySelector('#account-btn').onclick = () =>{
-    accountForm.classList.add('active');
+loginBtn.onclick = () => {
+  registerBtn.classList.remove("active");
+  loginBtn.classList.add("active");
+  document.querySelector(".account-form .login-form").classList.add("active");
+  document
+    .querySelector(".account-form .register-form")
+    .classList.remove("active");
 };
 
-document.querySelector('#close-form').onclick = () =>{
-    accountForm.classList.remove('active');
+let accountForm = document.querySelector(".account-form");
+
+document.querySelector("#account-btn").onclick = () => {
+  accountForm.classList.add("active");
+};
+
+document.querySelector("#close-form").onclick = () => {
+  accountForm.classList.remove("active");
 };
