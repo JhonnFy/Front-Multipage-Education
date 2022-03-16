dbf6dc6e52f78a1333abc3bf0d9938ddaa372988
DijhonsDeveloper <dijhonsdeveloper1023@gmail.com>
2022-03-15 23:49:22 -0500

    TeachersOk

--- js/script.js
+++ js/script.js
@@ -67,3 +67,20 @@ var swiper = new Swiper(".home-courses-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".teachers-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    991:{
+      slidesPerView: 3,
+    },
+  },
+});
